a4c64738a1f0ddf0641c01309055ebfb22094e54
DESKTOP-O525QDI\czbas <czbas23@gmail.com>
2017-11-08 11:28:46 +0700

    fff

--- webhook.php
+++ webhook.php
@@ -1,4 +1,25 @@
 <?php
-file_put_contents('webhook.txt', json_encode($_POST));
-file_put_contents('webhook.json', $_POST['payload']);
+$arr_url = [
+  'https://thevirus.in.th:8443/modules/git/public/web-hook.php?uuid=b9f6bd2f-db56-bb35-c08c-494a990409b9'
+];
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+// we are doing a POST request
+curl_setopt($ch, CURLOPT_POST, 1);
+// adding the post variables to the request
+curl_setopt($ch, CURLOPT_POSTFIELDS, $_POST);
+
+$output = curl_exec($ch);
+
+foreach ($arr_url as $key => $value) {
+  curl_setopt($ch, CURLOPT_URL, $value);
+  $output = curl_exec($ch);
+  var_dump($output);
+}
+
+curl_close($ch);
+
+
 ?>
