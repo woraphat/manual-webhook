257097a5b275ee00a4d4cd4991acf32f67f74e60
DESKTOP-O525QDI\czbas <czbas23@gmail.com>
2017-11-08 10:31:36 +0700

    bbb

--- webhook.php
+++ webhook.php
@@ -1,6 +1,3 @@
 <?php
-$result = file_put_contents(__DIR__ . '/webhook.txt', json_encode($_POST).'555');
-if ($result) {
-  file_put_contents(__DIR__ . '/webhook.txt', 'put content success.');
-}
+$result = file_put_contents(__DIR__ . '/webhook.txt', json_encode($_POST));
 ?>
