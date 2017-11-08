60c0c2cf26abb31a28a56e03fb3944c23fa4c2de
DESKTOP-O525QDI\czbas <czbas23@gmail.com>
2017-11-08 11:06:13 +0700

    bbb

--- webhook.php
+++ webhook.php
@@ -2,7 +2,7 @@
 if (count($_POST)) {
   $str = json_encode($_POST);
   if (is_string($str)) {
-    $result = file_put_contents(__DIR__ . '/webhookcc.txt', $str);
+    $result = file_put_contents(__DIR__ . '/webhook.txt', $str);
   }
 }
 ?>
